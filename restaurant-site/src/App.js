b2c9198c205e95cdbd497ee75e39e9ab71c05812
Jakub <stasiewski.jakub@gmail.com>
2023-02-15 23:22:03 +0100

    loading screen

--- restaurant-site/src/App.js
+++ restaurant-site/src/App.js
@@ -1,9 +1,29 @@
+import React, { useState, useEffect } from 'react';
 import Navbar from './components/Navbar'
+import ClipLoader from 'react-spinners/ClipLoader'
 
 function App() {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true)
+    setTimeout(()=>{
+      setLoading(false);
+    },1000)
+  },[]);
+
   return (
     <div>
-      <Navbar/>
+      {
+        loading 
+        ?(
+          <div className='flex justify-center items-center h-screen'>
+            <ClipLoader size={150} color={"#000"} loading={loading}/>
+          </div>
+        )
+        
+        :(<Navbar/>)
+      }
     </div>
   );
 }
