c95799ba1463d723a8503b135bbb973bc7a0429b
Jakub <stasiewski.jakub@gmail.com>
2023-02-15 22:51:07 +0100

    placing elements on navbar

--- restaurant-site/src/components/Navbar.jsx
+++ restaurant-site/src/components/Navbar.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import logo from '../photos/logo.png'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='bg-black flex justify-between'>
+      <div className='w-1/4'/>
+      <img src={logo}/>
+      <div className='flex justify-end text-white self-center text-right w-1/4'>
+        <button className='flex px-3 py-2 m-2 rounded-md hover:bg-gray-900 border-solid border-2 border-white'>Menu</button>
+      </div>
+    </div>
   )
 }
 
